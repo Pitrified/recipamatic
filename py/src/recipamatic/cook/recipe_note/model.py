354796c59d64f88c095698edbe7e66965c4124e4
Pitrified <nobilipietro@gmail.com>
2024-12-29 21:40:03 +0100

    Add RecipeNote model for managing cooking notes

---
+++ py/src/recipamatic/cook/recipe_note/model.py
@@ -0,0 +1,39 @@
+"""Take notes while cooking a recipe."""
+
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class Note(BaseModel):
+    """Represents a note for a recipe."""
+
+    text: str = Field(
+        ...,
+        description="The text of the note.",
+    )
+    timestamp: datetime = Field(
+        default_factory=datetime.now,
+        description="The timestamp when the note was taken.",
+    )
+
+
+class RecipeNote(BaseModel):
+    """Represents a note for a recipe."""
+
+    start_timestamp: datetime = Field(
+        default_factory=datetime.now,
+        description="The timestamp when the note was taken.",
+    )
+    notes: list[Note] = Field(
+        ...,
+        description="A list of notes for the recipe.",
+    )
+
+    def add_note(self, text: str) -> None:
+        """Add a note to the recipe."""
+        note = Note(
+            text=text,
+        )
+        self.notes.append(note)
