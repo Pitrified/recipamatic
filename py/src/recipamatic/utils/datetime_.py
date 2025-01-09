833c572e8f959e0c81139f71b3aa1dc604fea3dd
Pitrified <nobilipietro@gmail.com>
2025-01-09 21:59:46 +0100

    Add utility function to format timedelta as minutes and seconds

---
+++ py/src/recipamatic/utils/datetime_.py
@@ -0,0 +1,9 @@
+"""Utils for datetime operations."""
+
+from datetime import timedelta
+
+
+def format_time_delta(delta: timedelta) -> str:
+    """Format a timedelta as minutes and seconds."""
+    delta_f = f"{delta.seconds // 60}m {delta.seconds % 60}s"
+    return delta_f
