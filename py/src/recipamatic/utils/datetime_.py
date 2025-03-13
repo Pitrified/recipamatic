c78eb6e26b49c83e2b3c4efcf7170c8d85ffc920
Pitrified <nobilipietro@gmail.com>
2025-03-13 22:10:24 +0100

    Add timestamp generation utility to datetime module

--- py/src/recipamatic/utils/datetime_.py
+++ py/src/recipamatic/utils/datetime_.py
@@ -1,12 +1,26 @@
 """Utils for datetime operations."""
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 def format_time_delta(delta: timedelta) -> str:
     """Format a timedelta as minutes and seconds."""
     delta_f = f"{delta.seconds // 60}m {delta.seconds % 60}s"
     return delta_f
+
+
+def generate_timestamp_code() -> str:
+    """Generate a formatted timestamp code using the current datetime.
+
+    Returns:
+        str: A timestamp string formatted according to FMT_DATETIME
+
+    Note:
+        This function may produce duplicate codes if called multiple times within
+        the same second.
+    """
+    timestamp: datetime = datetime.now()
+    return timestamp.strftime(FMT_DATETIME)
 
 
 FMT_DATETIME = "%Y%m%d_%H%M%S"
